ac3f579b60669a69cb63291f969136b5514ad610
Remek <remigiusz.poltorak@gmail.com>
2015-12-28 12:15:30 +0100

    Added item removal

--- src/app/items/item-list.component.ts
+++ src/app/items/item-list.component.ts
@@ -3,9 +3,23 @@ import {IItem, Item} from './item';
 
 @Component({
     selector: 'item-list',
+    inputs: ['items'],
     templateUrl: './app/items/item-list.html'
 })
 
 export class ItemList {
-    @Input() items: IItem[];
+    public items: IItem[];
+    
+    public remove(id: number): IItem {
+        let i: number = this.items.length;
+        
+        while(i--) {
+            let item: IItem = this.items[i];
+            
+            if (item.id === id) {
+                this.items.splice(i, 1);
+                return item;    
+            }
+        }
+    }
 }
